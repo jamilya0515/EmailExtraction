1d8bcd43e50f29720e92678d1c59fecf35d224d0
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-01-21 15:20:01 +0000

    Part 2, count with Regex

--- emailExtract.js
+++ emailExtract.js
@@ -1,14 +1,29 @@
 const fs = require('fs');
-fs.readFile('test.txt', function(err, data) {
-    let input = data.toString();
-    let inpArr = input.split(' ');
-    var counter = 0;
-    for(var i = 0; i < inpArr.length; i++) {
-        if (inpArr[i].includes('@softwire.com'))
-        { 
-            
-            counter = counter + 1 ;
+function countEmailAddrWithoutRegExp(){
+    fs.readFile('test.txt', function(err, data) {
+        let input = data.toString();
+        let inpArr = input.split(' ');
+        var counter = 0;
+        for(var i = 0; i < inpArr.length; i++) {
+            if (inpArr[i].includes('@softwire.com'))
+            { 
+                
+                counter = counter + 1 ;
+            }
         }
-    }
-    console.log(counter);
-});
+        console.log(counter);
+    });
+
+}
+function countEmailAddrWithRegExp(){
+    let matchLen = 0;
+    fs.readFile('test.txt', function(err, data) {
+         let input = data.toString();
+        // let patternRegEx = /[A-Za-z0-9._]+@softwire.com/gi;
+        let patternRegEx = /\w+@techswitch.co.uk/gi;       
+        matchLen = input.match(patternRegEx).length;
+         console.log(matchLen);
+    });
+    
+}    
+countEmailAddrWithRegExp();
